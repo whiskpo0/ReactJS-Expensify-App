399433672acaa5a9ecdd0ac4e988898170372a0c
whiskpo0 <whiskypo082@gmail.com>
2019-02-19 12:25:02 -0600

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import React from 'react'; 
-
-const Header = (props) => (
-    <div className="header">
-        <div className="container">
-            <h1 className="header__title">{props.title}</h1>
-            {props.subTitle && <h2 className="header__subtitle">{props.subTitle}</h2>}
-        </div>
-    </div> 
-);
-
-Header.defaultProps = { 
-    title: 'Indecision'
-}; 
+import { NavLink } from 'react-router-dom';
+const Header = () => ( 
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
+    </header>
+); 
 
 export default Header; 
